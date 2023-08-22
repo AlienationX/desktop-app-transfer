5090b1f780614f3e36dd3ecbf145575ac8297231
lshu <>
2023-08-22 17:59:46 +0800

    update

--- transfer/ui/transfer_ui.py
+++ transfer/ui/transfer_ui.py
@@ -17,15 +17,31 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(501, 286)
-        self.horizontalLayout = QHBoxLayout(Form)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        Form.resize(585, 455)
+        self.gridLayout_2 = QGridLayout(Form)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
+        self.pushButton_5 = QPushButton(Form)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+
+        self.gridLayout.addWidget(self.pushButton_5, 3, 1, 1, 1)
+
+        self.checkBox = QCheckBox(Form)
+        self.checkBox.setObjectName(u"checkBox")
+
+        self.gridLayout.addWidget(self.checkBox, 4, 1, 1, 1)
+
         self.pushButton_3 = QPushButton(Form)
         self.pushButton_3.setObjectName(u"pushButton_3")
 
         self.gridLayout.addWidget(self.pushButton_3, 2, 0, 1, 1)
+
+        self.pushButton_4 = QPushButton(Form)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setEnabled(False)
+
+        self.gridLayout.addWidget(self.pushButton_4, 2, 1, 1, 1)
 
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
@@ -45,17 +61,7 @@ class Ui_Form(object):
         self.pushButton_6 = QPushButton(Form)
         self.pushButton_6.setObjectName(u"pushButton_6")
 
-        self.gridLayout.addWidget(self.pushButton_6, 5, 0, 1, 1)
-
-        self.radioButton = QRadioButton(Form)
-        self.radioButton.setObjectName(u"radioButton")
-
-        self.gridLayout.addWidget(self.radioButton, 4, 0, 1, 1)
-
-        self.pushButton_5 = QPushButton(Form)
-        self.pushButton_5.setObjectName(u"pushButton_5")
-
-        self.gridLayout.addWidget(self.pushButton_5, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_6, 4, 0, 1, 1)
 
         self.label = QLabel(Form)
         self.label.setObjectName(u"label")
@@ -68,27 +74,35 @@ class Ui_Form(object):
 
         self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
 
-        self.pushButton_4 = QPushButton(Form)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-
-        self.gridLayout.addWidget(self.pushButton_4, 2, 1, 1, 1)
-
         self.lineEdit = QLineEdit(Form)
         self.lineEdit.setObjectName(u"lineEdit")
 
-        self.gridLayout.addWidget(self.lineEdit, 6, 0, 1, 1)
+        self.gridLayout.addWidget(self.lineEdit, 5, 0, 1, 1)
 
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.groupBox = QGroupBox(Form)
+        self.groupBox.setObjectName(u"groupBox")
+        self.verticalLayout_2 = QVBoxLayout(self.groupBox)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.radioButton = QRadioButton(self.groupBox)
+        self.radioButton.setObjectName(u"radioButton")
 
-        self.gridLayout.addItem(self.verticalSpacer, 3, 0, 1, 1)
+        self.verticalLayout.addWidget(self.radioButton)
 
-        self.checkBox = QCheckBox(Form)
-        self.checkBox.setObjectName(u"checkBox")
+        self.radioButton_2 = QRadioButton(self.groupBox)
+        self.radioButton_2.setObjectName(u"radioButton_2")
 
-        self.gridLayout.addWidget(self.checkBox, 5, 1, 1, 1)
+        self.verticalLayout.addWidget(self.radioButton_2)
 
 
-        self.horizontalLayout.addLayout(self.gridLayout)
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+
+        self.gridLayout.addWidget(self.groupBox, 3, 0, 1, 1)
+
+
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
 
 
         self.retranslateUi(Form)
@@ -98,14 +112,16 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Form", u"PushButton5", None))
+        self.checkBox.setText(QCoreApplication.translate("Form", u"CheckBox", None))
         self.pushButton_3.setText(QCoreApplication.translate("Form", u"PushButton3", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Form", u"PushButton4", None))
         self.pushButton_1.setText(QCoreApplication.translate("Form", u"PushButton1", None))
         self.pushButton_2.setText(QCoreApplication.translate("Form", u"PushButton2", None))
         self.pushButton_6.setText(QCoreApplication.translate("Form", u"PushButton6", None))
-        self.radioButton.setText(QCoreApplication.translate("Form", u"RadioButton", None))
-        self.pushButton_5.setText(QCoreApplication.translate("Form", u"PushButton5", None))
         self.label.setText(QCoreApplication.translate("Form", u"TextLabel", None))
-        self.pushButton_4.setText(QCoreApplication.translate("Form", u"PushButton4", None))
-        self.checkBox.setText(QCoreApplication.translate("Form", u"CheckBox", None))
+        self.groupBox.setTitle(QCoreApplication.translate("Form", u"GroupBox", None))
+        self.radioButton.setText(QCoreApplication.translate("Form", u"RadioButton", None))
+        self.radioButton_2.setText(QCoreApplication.translate("Form", u"RadioButton", None))
     # retranslateUi
 
--- transfer/widgets/excel_widget.py
+++ transfer/widgets/excel_widget.py
@@ -15,11 +15,18 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Slot, Qt, QSize, QStandardPaths, QUrl, QFile, QSaveFile, QDir, QIODevice
 from PySide6.QtGui import QFont
 
+import pandas as pd
+
+from pathlib import Path
+
 
 class ExcelToCsvWidget(QWidget):
     
     def __init__(self):
         super().__init__()
+        self.inputFiles = []
+        self.outputPath = ""
+        
         self.setupUi()
     
     def setupUi(self):
@@ -33,9 +40,9 @@ class ExcelToCsvWidget(QWidget):
         self.label.setFont(font)
         self.label.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
         
-        self.inputFile = QLineEdit()
-        self.inputFile.setPlaceholderText("请选择Excel文件")
-        self.inputFile.setReadOnly(True)
+        self.inputLineEdit = QLineEdit()
+        self.inputLineEdit.setPlaceholderText("请选择Excel文件或目录")
+        self.inputLineEdit.setReadOnly(True)
         self.openBtn = QPushButton("打开文件")
 
         self.radioBtn1 = QRadioButton("单个文本使用双引号转义")
@@ -46,10 +53,18 @@ class ExcelToCsvWidget(QWidget):
         self.radioBtn3 = QRadioButton("不带双引号")
         self.radioBtn3.setToolTip("所有字段都用逗号分隔，如果内容存在逗号，打开文件可能会导致串列问题")
         
-        self.outputFile = QLineEdit()
-        self.outputFile.setPlaceholderText("请选择保存的路径")
-        self.outputFile.setReadOnly(True)
+        self.enterCheckBox = QCheckBox("包括子文件夹下的Excel文件")
+        
+        self.outputLineEdit = QLineEdit()
+        self.outputLineEdit.setPlaceholderText("请选择保存的路径")
+        self.outputLineEdit.setReadOnly(True)
         self.saveBtn = QPushButton("保存文件")
+        
+        self.radioBtn4 = QRadioButton("UTF8")
+        self.radioBtn4.setToolTip("UTF8编码")
+        self.radioBtn4.setChecked(True)
+        self.radioBtn5 = QRadioButton("ANSI/GBK")
+        self.radioBtn5.setToolTip("中文编码")
         
         self.overwriteCheckBox = QCheckBox("覆盖已存在文件")
         
@@ -62,12 +77,12 @@ class ExcelToCsvWidget(QWidget):
         self.gridLayout = QGridLayout()
         # 将组件添加到布局中
         self.gridLayout.addWidget(self.label, 1, 1)
-        self.gridLayout.addWidget(self.inputFile, 2, 1)
+        self.gridLayout.addWidget(self.inputLineEdit, 2, 1)
         self.gridLayout.addWidget(self.openBtn, 2, 2)
         self.gridLayout.addWidget(self.radioBtn1, 3, 1)
         self.gridLayout.addWidget(self.radioBtn2, 4, 1)
         self.gridLayout.addWidget(self.radioBtn3, 5, 1)
-        self.gridLayout.addWidget(self.outputFile, 6, 1)
+        self.gridLayout.addWidget(self.outputLineEdit, 6, 1)
         self.gridLayout.addWidget(self.saveBtn, 6, 2)
         self.gridLayout.addWidget(self.overwriteCheckBox, 7, 1)
         self.gridLayout.addWidget(self.startBtn, 8, 1, 1, 2)
@@ -76,7 +91,71 @@ class ExcelToCsvWidget(QWidget):
 
         # 为窗体添加布局
         self.setLayout(self.gridLayout)
+        
+        # 添加点击事件
+        self.openBtn.clicked.connect(self.open)
+        self.saveBtn.clicked.connect(self.save)
+        self.startBtn.clicked.connect(self.execute)
+        
+    @Slot()
+    def open(self):
+        dir_path = QFileDialog.getExistingDirectory(
+            self, "Open Directory Or File", QDir.homePath(), QFileDialog.ShowDirsOnly
+        )
 
+        if dir_path:
+            dest_dir = QDir(dir_path)
+            self.inputLineEdit.setText(QDir.fromNativeSeparators(dest_dir.path()))
+            for item in dest_dir.entryList():
+                extension = item.split(".")[-1]
+                if extension in ["xlsx", "xls"]:
+                    file = dest_dir.filePath(item)
+                    self.inputFiles.append(file)
+            self.gridLayout.addWidget(self.enterCheckBox, 3, 2)
+    
+    @Slot()
+    def save(self):
+        dir_path = QFileDialog.getExistingDirectory(
+            self, "Open Directory", QDir.homePath(), QFileDialog.ShowDirsOnly
+        )
+
+        if dir_path:
+            dest_dir = QDir(dir_path)
+            self.outputLineEdit.setText(QDir.fromNativeSeparators(dest_dir.path()))
+            self.outputPath = dest_dir.path()
+    
+    @Slot()
+    def execute(self):
+        print("inputFiles = ", self.inputFiles)
+        print("outputPath = ", self.outputPath)
+        self.inputFiles =  [r'C:/Users/Admin/Desktop/in/副本赣州市皮肤病医院（赣州市皮肤病研究所、赣州市麻风病康复中心、赣州市性病防治中心）-医保-src-彩色多普勒超声-收费项目明细-20230821172226247.xlsx', r'C:/Users/Admin/Desktop/in/北京一体机开发环境.xlsx']
+        self.outputPath = "C:/Users/Admin/Desktop/out"
+        if not self.inputFiles:
+            QMessageBox.warning(self, "Input Error", "请选择Excel文件或目录")
+            return
+        if not self.outputPath:
+            QMessageBox.warning(self, "Output Error", "请选择保存的路径")
+            return
+        
+        self.startBtn.setText("执行中......")
+        self.startBtn.setEnabled(False)
+        self.openBtn.setEnabled(False)
+        self.saveBtn.setEnabled(False)
+        
+        for file in self.inputFiles:
+            from_file =  file
+            to_file = Path(self.outputPath).joinpath(Path(file).name)
+            print(from_file, to_file)
+            self.excelToCsv(from_file, to_file)
+            
+        self.startBtn.setText("执行")
+        self.startBtn.setEnabled(True)
+        self.openBtn.setEnabled(True)
+        self.saveBtn.setEnabled(True)
+        
+    def excelToCsv(self, from_file, to_file):
+        df = pd.read_excel(from_file)
+        df.to_csv(to_file)
 
 class ExcelSplitWidget(QWidget):
     
